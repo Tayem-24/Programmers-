6b428ab0e0162ce88e400d40a6a8e25d34c8728c
Tayem-24 <138823317+Tayem-24@users.noreply.github.com>
2024-08-13 23:37:12 +0300

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+app = Tk()
+app.geometry("500x500+400+200")
+app.resizable(True ,False)
+app.title("app h")
+app.config(background= "red")
+app.minsize(200,200)
+#app.mixsize(800,800)
+app.mainloop()
