a4c12bd6eda0ce16a13001ae530ca78606c407ac
Tayem-24 <tayem.farouk.222@gmail.com>
2024-08-15 05:18:14 +0300

    Delete community.py

--- app/community.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-import tkinter as tk
-commu =
-home_screen.state('normal')
-    home_screen.title("Home")
-    home_screen.geometry("360x640")
-    home_screen.configure(bg="#f0f0f0")
